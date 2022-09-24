8aad1ff8c2b449049b5f468a8fed14bc14514479
DrMoE77 <drmo.afifi@gmail.com>
2022-09-24 19:11:30 +1000

    btncaps

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 
-const choiceBtns = document.querySelectorAll(".choicebtn")
+const choiceBtns = document.querySelectorAll(".choiceBtn")
 
 let player;
 let computer;
 let result;
 
 choiceBtns.forEach(button => button.addEventListener("click", ()=> {
+    console.lo
     player = button.textContent;
     computerTurn();
     playerText.textContent = 'Player: ${player}';
